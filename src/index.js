6cbfd2755bcfc63dcd4ac4c61d56f0932f29f958
ppaul.aritra <ppaul.aritra@gmail.com>
2020-01-14 22:46:09 +0600

    tried to implement state in main.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import Main from './components/Main';
+import './style/stylesheet.css';
+
+ReactDOM.render(<Main/>,document.getElementById('root'));
+
+
