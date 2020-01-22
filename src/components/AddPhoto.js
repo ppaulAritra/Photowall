39781be4ea2d5cd0aabcfac86c0c46c00068a29d
ppaul.aritra <ppaul.aritra@gmail.com>
2020-01-22 18:42:55 +0600

    button for adding new phtos

---
+++ src/components/AddPhoto.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react'
+
+class AddPhoto extends Component{
+    render(){
+        return <h1>Add photo</h1>
+    }
+}
+export default AddPhoto
--- src/components/photowall.js
+++ src/components/photowall.js
@@ -2,16 +2,20 @@ import React from 'react';
 import Photo from './photo';
 import PropTypes from 'prop-types'
 
-function Photowall(props){
-    return <div className='photoGrid'>
-                
-    {props.posts.map((post,index)=> <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>)}
+function Photowall(props) {
+    return <div>
+        <a onClick={props.onNavigate} className="addIcon" href="#AddPhoto"></a>
+        {/* <button onClick={props.onNavigate} className="addIcon"></button> */}
+            <div className='photoGrid'>
 
-</div>
+            {props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto} />)}
+
+            </div>
+    </div>
 }
-Photowall.propTypes={
-    posts:PropTypes.array.isRequired,
-    onRemovePhoto:PropTypes.func.isRequired
+Photowall.propTypes = {
+    posts: PropTypes.array.isRequired,
+    onRemovePhoto: PropTypes.func.isRequired
 }
 
 export default Photowall
